b6f341d528642b1a62ef291cc4beb90b79fac85d
Kostantinos Giokotos <64098294+kostasgioko@users.noreply.github.com>
2021-01-28 14:32:28 +0200

    Update photographObject.m

--- Part 2 - Transformations and Projections/photographObject.m
+++ Part 2 - Transformations and Projections/photographObject.m
@@ -1,4 +1,17 @@
 function [P2d, D] = photographObject(p, M, N, H, W, w, cv, ck, cu)
+%Inputs:
+%p: 3D coordinates of the points.
+%M, N: Image dimensions.
+%H, W: Camera panel dimensions.
+%w: Focal length of the camera.
+%cv: Center of camera coordinates in terms of WCS.
+%ck: Camera target point coordinates.
+%cu: Camera up vector coordinates.
+
+%Outputs:
+%P2d: Pixel coordinates of the points.
+%D: Depths of the points.
+
     %This function takes the point coordinates from the WCS, finds their camera projections and depths and rasterizes the image.
     
     %Get the point projections and depths.
